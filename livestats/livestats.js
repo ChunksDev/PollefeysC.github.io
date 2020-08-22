d7996683cafd0412bae17e2c694416b4874da6ac
Cedric <cedric.pollefeys@gmail.com>
2020-08-22 23:51:25 +0200

    livestats visuals left

--- livestats/livestats.js
+++ livestats/livestats.js
@@ -1,5 +1,6 @@
 let whitelist = ["e7de1ed5ec6d3d2d4f37d9142785ba40"];
 let keyOwnerUuid = null;
+let ownerIGN = null;
 let apikey = null;
 let initial = new Map();
 let current = new Map();
@@ -20,6 +21,7 @@ function initStats() {
         .then(result => result.json())
         .then(({ record }) => {
             keyOwnerUuid = record.owner;
+            document.getElementsByTagName("H4").item(0).innerHTML = "You're <strong>1</strong> out of <strong>" + whitelist.length + "</strong> whitelisted players";
             getSessionStartData();
             sessionLoop();
         })
@@ -29,6 +31,12 @@ function getSessionStartData() {
     fetch("https://api.hypixel.net/player?uuid="+keyOwnerUuid+"&key="+apikey)
         .then(result => result.json())
         .then(({ player }) => {
+            ownerIGN = player.displayname;
+            if (ownerIGN.substring(ownerIGN.length-1) == "s") {
+                document.getElementsByTagName("H1").item(0).innerHTML = ownerIGN + "' livestats" + " <img src='http://cravatar.eu/helmavatar/" + keyOwnerUuid + "/24.png'>";
+            } else {
+                document.getElementsByTagName("H1").item(0).innerHTML = ownerIGN + "'s livestats" + " <img src='http://cravatar.eu/helmavatar/" + keyOwnerUuid + "/24.png'>";
+            }
             let stats = player.stats.SkyWars;
             initial.set("wins", stats.wins);
             initial.set("losses", stats.losses);
@@ -43,8 +51,7 @@ function getSessionStartData() {
 
 async function sessionLoop() {
     getSessionCurrentData();
-    updateVisuals();
-    await wait(5000);
+    await wait(3000);
     if (continueSession){
         await sessionLoop();
     }
@@ -63,11 +70,56 @@ function getSessionCurrentData() {
             current.set("experience", stats.skywars_experience);
             current.set("heads", stats.heads);
             current.set("shard", stats.shard);
+            updateVisuals();
         })
 }
 
 function updateVisuals() {
+    document.getElementById("wins").innerHTML = current.get("wins");
+        document.getElementById("swins").innerHTML = "(+" + (current.get("wins")-initial.get("wins")).toString() + ")";
 
+    document.getElementById("losses").innerHTML = current.get("losses");
+        document.getElementById("slosses").innerHTML = "(+" + (current.get("losses")-initial.get("losses")).toString() + ")";
+
+    document.getElementById("winpercentage").innerHTML = getCurrentWinPercentage() + "%";
+        document.getElementById("swinpercentage").innerHTML = "(" + getSessionWinPercentage() + ")";
+
+    document.getElementById("kills").innerHTML = current.get("kills");
+        document.getElementById("skills").innerHTML = "(+" + (current.get("kills")-initial.get("kills")).toString() + ")";
+
+    document.getElementById("deaths").innerHTML = current.get("deaths");
+        document.getElementById("sdeaths").innerHTML = "(+" + (current.get("deaths")-initial.get("deaths")).toString() + ")";
+
+    document.getElementById("kd").innerHTML = getCurrentKD().toString();
+        document.getElementById("skd").innerHTML = "(" + getSessionKD() + ")";
+}
+
+function getCurrentWinPercentage() {
+    if (current.get("losses")+current.get("wins") == 0) {
+        return "N/A";
+    }
+    return Math.round(current.get("wins")/(current.get("losses")+current.get("wins"))*10000)/100;
+}
+
+function getSessionWinPercentage() {
+    if ((current.get("losses")-initial.get("losses"))+(current.get("wins")-initial.get("wins")) == 0) {
+        return "N/A";
+    }
+    return Math.round((current.get("wins")-initial.get("wins"))/((current.get("losses")-initial.get("losses"))+(current.get("wins")-initial.get("wins")))*10000)/100;
+}
+
+function getCurrentKD() {
+    if (current.get("deaths") == 0) {
+        return "&infin;";
+    }
+    return Math.round(current.get("kills")/current.get("deaths")*1000)/1000;
+}
+
+function getSessionKD() {
+    if (current.get("deaths")-initial.get("deaths") == 0) {
+        return "&infin;";
+    }
+    return Math.round((current.get("kills")-initial.get("kills"))/(current.get("deaths")-initial.get("deaths"))*1000)/1000;
 }
 
 function wait(ms) {
